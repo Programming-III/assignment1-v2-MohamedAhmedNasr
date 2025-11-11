853cf2a84c3b151ce92e6d0072a8633d2ffa39a5
MohamedAhmedNasr <hamada.ahmed2006.ma21@gmail.com>
2025-11-11 09:56:44 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -4,6 +4,19 @@
 #include "Person.h"
 using namespace std;
 //#write Instructor class here
+class person {
+private:
+    string name;
+    int id ;
+    public:
+    person();
+    person(string name, int id);
+    void  display() ;
+    string get_name();
+    int get_id();
+    ~person();
+
+};
 
 
 
